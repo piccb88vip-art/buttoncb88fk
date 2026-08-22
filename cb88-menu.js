c0ecdf93c565dfc3ed7cdfc3f9b1166f21195972
piccb88vip-art <pic_cb88vip@clickgroup.id>
2026-08-22 16:29:00 +0700

    Update cb88-menu.js

--- cb88-menu.js
+++ cb88-menu.js
@@ -22,7 +22,7 @@
 
             <strong>CARI TAMPILAN SBOBET RESMI?</strong>
 
-            <p>Mainkan sportsbook favoritmu dengan pilihan favorit kamu!</p>
+            <p>Mainkan sportsbook favoritmu dengan pilihan</p>
 
             <div class="cb88-provider-grid">
               <div class="cb88-provider">
