547046f073459b5ba7faa94d3123e1049c3b07bd
piccb88vip-art <pic_cb88vip@clickgroup.id>
2026-08-24 08:31:11 +0700

    Update site-header.js

--- site-header.js
+++ site-header.js
@@ -3,7 +3,7 @@
   .c-ptpKx{
     position: fixed !important;
     overflow: hidden !important;
-    background: url("https://plcl.me/images/pJqHY.jpg") center center / cover no-repeat !important;
+    background: url("https://plcl.me/images/C7M3m.jpg") center center / cover no-repeat !important;
   }
 
   .c-ptpKx img[alt="header-bg"]{
